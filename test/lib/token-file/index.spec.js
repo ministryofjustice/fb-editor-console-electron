40a4dba510d2a17baeb1b83d1aa18309ce9a473f
Jonathan Perry <jonathanperry@sequencemedia.net>
2020-02-13 12:36:17 +0000

    `encryptToken` `decryptToken`

--- test/lib/token-file/index.spec.js
+++ test/lib/token-file/index.spec.js
@@ -12,6 +12,11 @@ const {
 } = chai
 
 const mockAccess = sinon.stub()
+const mockEncrypt = sinon.stub()
+const mockDecrypt = sinon.stub()
+
+const mockWriteFile = sinon.stub()
+const mockReadFile = sinon.stub()
 
 const {
   hasTokenFile,
@@ -26,11 +31,14 @@ const {
     constants: {
       F_OK: 'mock okay'
     },
-    readFile: {},
-    writeFile: {}
+    readFile: mockReadFile,
+    writeFile: mockWriteFile
   },
   'electron-timber': timber,
-  './crypto': {}
+  './crypto': {
+    encrypt: mockEncrypt,
+    decrypt: mockDecrypt
+  }
 })
 
 describe('~/fb-editor-console-electron/lib/token-file', () => {
@@ -72,5 +80,93 @@ describe('~/fb-editor-console-electron/lib/token-file', () => {
         return expect(await hasTokenFile()).to.be.false
       })
     })
+  })
+
+  describe('`encryptToken()`', () => {
+    const mockStringify = sinon.stub()
+    const mockFrom = sinon.stub()
+    const mockBuffer = {}
+
+    beforeEach(async () => {
+      mockEncrypt.returns('mock encrypted token')
+      mockStringify.returns('mock json')
+      mockFrom.returns(mockBuffer)
+
+      global.JSON = {
+        stringify: mockStringify
+      }
+
+      global.Buffer = {
+        from: mockFrom
+      }
+
+      await encryptToken('mock token', 'mock password')
+    })
+
+    afterEach(() => {
+      delete global.JSON
+
+      delete global.Buffer
+    })
+
+    it('assigns the token to the `token` field of an object and serialises it to JSON', () => {
+      expect(mockStringify).to.be.calledWith({ token: 'mock token' })
+    })
+
+    it('creates a buffer from the JSON', () => {
+      expect(mockFrom).to.be.calledWith('mock json')
+    })
+
+    it('encrypts the token buffer', () => {
+      expect(mockEncrypt).to.be.calledWith(mockBuffer, 'mock password')
+    })
+
+    it('writes the token file', () => {
+      expect(mockWriteFile).to.be.calledWith('./.token', 'mock encrypted token')
+    })
+  })
+
+  describe('`decryptToken()`', () => {
+    const mockReadFileBuffer = {}
+    const mockDecryptBuffer = {
+      toString: sinon.stub().returns('mock json')
+    }
+    const mockParse = sinon.stub()
+    const mockFrom = sinon.stub()
+    let returnValue
+
+    beforeEach(async () => {
+      mockReadFile.returns(mockReadFileBuffer)
+      mockDecrypt.returns(mockDecryptBuffer)
+      mockParse.returns({ token: 'mock token' })
+
+      global.JSON = {
+        parse: mockParse
+      }
+
+      global.Buffer = {
+        from: mockFrom
+      }
+
+      returnValue = await decryptToken('mock password')
+    })
+
+    afterEach(() => {
+      delete global.JSON
+
+      delete global.Buffer
+    })
+
+    it('reads the token file', () => {
+      expect(mockReadFile).to.be.calledWith('./.token')
+    })
+
+    it('decrypts the file buffer', () => {
+      expect(mockDecrypt).to.be.calledWith(mockReadFileBuffer, 'mock password')
+    })
+
+    it('deserialises the JSON to an object and destructures the `token` field', () => expect(mockParse).to.be.calledWith('mock json'))
+
+    it('returns the token', () => expect(returnValue).to.equal('mock token'))
   })
 })
