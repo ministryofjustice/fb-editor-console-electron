40d0fe934ec5694b3a2fa96d5e7148ef77d9bc83
matt tei <matt.tei@digital.justice.gov.uk>
2020-08-11 16:07:07 +0100

    Update nvs.bat

--- lib/nvs.bat
+++ lib/nvs.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 title Installing Form Builder Editor
 ECHO Starting to install the editor.
-C:\Users\matttei\Documents\formbuilder\.nvs\nvs.cmd use 12.4.0
+C:\Users\matttei\Documents\formbuilder\.nvs\nvs.cmd use 12.4.0 # needs current path!
 ECHO node --version
 node --version
 PAUSE
