01d02317fb3473181d952ad008bd8d8f86e58dea
matttei <matt.tei@digital.justice.gov.uk>
2020-08-11 16:04:22 +0100

    Added scripts to install and update node in windows.

---
+++ lib/npm.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+title Installing Form Builder Editor
+ECHO current directory - %__CD__%
+cd "C:\Users\matttei\Documents\formbuilder\.editor"
+ECHO changed directory - %__CD__%
+ECHO start npm install
+npm install
+ECHO finished
+PAUSE
---
+++ lib/nvs.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+title Installing Form Builder Editor
+ECHO Starting to install the editor.
+C:\Users\matttei\Documents\formbuilder\.nvs\nvs.cmd use 12.4.0
+ECHO node --version
+node --version
+PAUSE
